8ba28cc024b22383e5fec9867296b48f50756822
Max Jansen <74352242+MaxJnsn@users.noreply.github.com>
2024-12-14 17:00:28 +0100

    Initial commit

---
+++ src/main/java/com/maxjnsn/spawnersplus/SpawnersPlus.java
@@ -0,0 +1,16 @@
+package com.maxjnsn.spawnersplus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpawnersPlus implements ModInitializer {
+	public static final String MOD_ID = "spawnersplus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/maxjnsn/spawnersplus/SpawnersPlusClient.java
@@ -0,0 +1,10 @@
+package com.maxjnsn.spawnersplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpawnersPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
